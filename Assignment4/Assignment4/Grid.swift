86608c16885580d8d66cea4104e3e0893d567631
Mustafa Rabie <mrabie@Mustafas-Mac.local>
2017-04-22 06:06:33 -0700

    updating

--- Assignment4/Assignment4/Grid.swift
+++ Assignment4/Assignment4/Grid.swift
@@ -1,56 +1,13 @@
 //
 //  Grid.swift
 //
-import Foundation
+public typealias GridPosition = (row: Int, col: Int)
+public typealias GridSize = (rows: Int, cols: Int)
 
-public typealias Position = (row: Int, col: Int)
-public typealias PositionSequence = [Position]
+fileprivate func norm(_ val: Int, to size: Int) -> Int { return ((val % size) + size) % size }
 
-// Implement the wrap around rules
-public func normalize(position: Position, to modulus: Position) -> Position {
-    let modRows = modulus.row, modCols = modulus.col
-    return Position(
-        row: ((position.row % modRows) + modRows) % modRows,
-        col: ((position.col % modCols) + modCols) % modCols
-    )
-}
-
-// Provide a sequence of all positions in a range
-public func positionSequence (from: Position, to: Position) -> PositionSequence {
-    return (from.row ..< to.row)
-        .map { row in zip( [Int](repeating: row, count: to.col - from.col), from.col ..< to.col ) }
-        .flatMap { $0 }
-}
-
-public enum CellState:String {
-    case alive = "alive"
-    case empty = "empty"
-    case born = "born"
-    case died = "died"
-    
-    public func description()->String{
-        return self.rawValue
-        }
-    
-    
-    /* NOTE TO PROF/TA: I would like to know which is the best/most efficient implementation. If you can give me feedback in canvas comments
-    I would really appreciate it. */
-    public func allValues()->Array<String>{
-        //let retArray = ["alive","empty","born","died"]
-        //let retArray = [CellState.alive.rawValue, CellState.empty.rawValue, CellState.born.rawValue, CellState.empty.rawValue]
-        //
-        //return retArray
-        return ["alive","empty","born","died"]
-    }
-    
-    public func toggle(value:CellState)-> CellState{
-        switch value{
-        case .empty, .died:
-             return .alive
-        case .alive, .born:
-             return .empty
-        }
-    }
+public enum CellState {
+    case alive, empty, born, died
     
     public var isAlive: Bool {
         switch self {
@@ -60,100 +17,88 @@ public enum CellState:String {
     }
 }
 
-public struct Cell {
-    var position = Position(row:0, col:0)
-    var state = CellState.empty
+public protocol GridProtocol {
+    init(_ rows: Int, _ cols: Int, cellInitializer: (GridPosition) -> CellState)
+    var description: String { get }
+    var size: GridSize { get }
+    subscript (row: Int, col: Int) -> CellState { get set }
+    func next() -> Self
 }
 
-public struct Grid {
-    private var _cells: [[Cell]]
-    fileprivate var modulus: Position { return Position(_cells.count, _cells[0].count) }
-    
-    // Get and Set cell states by position
-    public subscript (pos: Position) -> CellState {
-        get { let pos = normalize(position: pos, to: modulus); return _cells[pos.row][pos.col].state }
-        set { let pos = normalize(position: pos, to: modulus); _cells[pos.row][pos.col].state = newValue }
+public let lazyPositions = { (size: GridSize) in
+    return (0 ..< size.rows)
+        .lazy
+        .map { zip( [Int](repeating: $0, count: size.cols) , 0 ..< size.cols ) }
+        .flatMap { $0 }
+        .map { GridPosition($0) }
+}
+
+
+let offsets: [GridPosition] = [
+    (row: -1, col:  -1), (row: -1, col:  0), (row: -1, col:  1),
+    (row:  0, col:  -1),                     (row:  0, col:  1),
+    (row:  1, col:  -1), (row:  1, col:  0), (row:  1, col:  1)
+]
+
+extension GridProtocol {
+    public var description: String {
+        return lazyPositions(self.size)
+            .map { (self[$0.row, $0.col].isAlive ? "*" : " ") + ($0.col == self.size.cols - 1 ? "\n" : "") }
+            .joined()
     }
     
-    // Allow access to the sequence of positions
-    public let positions: PositionSequence
-    
-    // Initialize _cells and positions
-    public init(_ rows: Int, _ cols: Int, cellInitializer: (Position) -> CellState = { _, _ in .empty } ) {
-        _cells = [[Cell]]( repeatElement( [Cell](repeatElement(Cell(), count: rows)), count: cols) )
-        positions = positionSequence(from: Position(0,0), to: Position(rows, cols))
-        positions.forEach { _cells[$0.row][$0.col].position = $0; self[$0] = cellInitializer($0) }
+    private func neighborStates(of pos: GridPosition) -> [CellState] {
+        return offsets.map { self[pos.row + $0.row, pos.col + $0.col] }
     }
     
-    private static let offsets: [Position] = [
-        (row: -1, col:  -1), (row: -1, col:  0), (row: -1, col:  1),
-        (row:  0, col:  -1),                     (row:  0, col:  1),
-        (row:  1, col:  -1), (row:  1, col:  0), (row:  1, col:  1)
-    ]
-    private func neighbors(of position: Position) -> [CellState] {
-        return Grid.offsets.map {
-            let neighbor = normalize(position: Position(
-                row: (position.row + $0.row),
-                col: (position.col + $0.col)
-            ), to: modulus)
-            return self[neighbor]
+    private func nextState(of pos: GridPosition) -> CellState {
+        let iAmAlive = self[pos.row, pos.col].isAlive
+        let numLivingNeighbors = neighborStates(of: pos).filter({ $0.isAlive }).count
+        switch numLivingNeighbors {
+        case 2 where iAmAlive,
+             3: return iAmAlive ? .alive : .born
+        default: return iAmAlive ? .died  : .empty
         }
     }
     
-    private func nextState(of position: Position) -> CellState {
-        switch neighbors(of: position).filter({ $0.isAlive }).count {
-        case 2 where self[position].isAlive,
-             3: return self[position].isAlive ? .alive : .born
-        default: return self[position].isAlive ? .died  : .empty
-        }
-    }
-    
-    // Generate the next state of the grid
-    public func next() -> Grid {
-        var nextGrid = Grid(modulus.row, modulus.col)
-        positions.forEach { nextGrid[$0] = self.nextState(of: $0) }
+    public func next() -> Self {
+        var nextGrid = Self(size.rows, size.cols) { _, _ in .empty }
+        lazyPositions(self.size).forEach { nextGrid[$0.row, $0.col] = self.nextState(of: $0) }
         return nextGrid
     }
 }
 
-public extension Grid {
-    public var description: String {
-        return positions
-            .map { (self[$0].isAlive ? "*" : " ") + ($0.1 == self.modulus.col - 1 ? "\n" : "") }
-            .joined()
+public struct Grid: GridProtocol {
+    private var _cells: [[CellState]]
+    public let size: GridSize
+    
+    public subscript (row: Int, col: Int) -> CellState {
+        get { return _cells[norm(row, to: size.rows)][norm(col, to: size.cols)] }
+        set { _cells[norm(row, to: size.rows)][norm(col, to: size.cols)] = newValue }
     }
-    public var living: [Position] { return positions.filter { return  self[$0].isAlive   } }
-    public var dead  : [Position] { return positions.filter { return !self[$0].isAlive   } }
-    public var alive : [Position] { return positions.filter { return  self[$0] == .alive } }
-    public var born  : [Position] { return positions.filter { return  self[$0] == .born  } }
-    public var died  : [Position] { return positions.filter { return  self[$0] == .died  } }
-    public var empty : [Position] { return positions.filter { return  self[$0] == .empty } }
+    
+    public init(_ rows: Int, _ cols: Int, cellInitializer: (GridPosition) -> CellState = { _, _ in .empty }) {
+        _cells = [[CellState]](repeatElement( [CellState](repeatElement(.empty, count: rows)), count: cols))
+        size = GridSize(rows, cols)
+        lazyPositions(self.size).forEach { self[$0.row, $0.col] = cellInitializer($0) }
+    }
 }
 
 extension Grid: Sequence {
-    public struct SimpleGridIterator: IteratorProtocol {
-        private var grid: Grid
-        
-        public init(grid: Grid) {
-            self.grid = grid
-        }
-        
-        public mutating func next() -> Grid? {
-            grid = grid.next()
-            return grid
-        }
+    fileprivate var living: [GridPosition] {
+        return lazyPositions(self.size).filter { return  self[$0.row, $0.col].isAlive   }
     }
     
-    public struct HistoricGridIterator: IteratorProtocol {
+    public struct GridIterator: IteratorProtocol {
         private class GridHistory: Equatable {
-            let positions: [Position]
+            let positions: [GridPosition]
             let previous:  GridHistory?
             
             static func == (lhs: GridHistory, rhs: GridHistory) -> Bool {
                 return lhs.positions.elementsEqual(rhs.positions, by: ==)
             }
             
-            init(_ positions: [Position], _ previous: GridHistory? = nil) {
+            init(_ positions: [GridPosition], _ previous: GridHistory? = nil) {
                 self.positions = positions
                 self.previous = previous
             }
@@ -168,7 +113,7 @@ extension Grid: Sequence {
             }
         }
         
-        private var grid: Grid
+        private var grid: GridProtocol
         private var history: GridHistory!
         
         init(grid: Grid) {
@@ -176,24 +121,23 @@ extension Grid: Sequence {
             self.history = GridHistory(grid.living)
         }
         
-        public mutating func next() -> Grid? {
+        public mutating func next() -> GridProtocol? {
             if history.hasCycle { return nil }
-            let newGrid = grid.next()
+            let newGrid:Grid = grid.next() as! Grid
             history = GridHistory(newGrid.living, history)
             grid = newGrid
             return grid
         }
     }
     
-    public func makeIterator() -> HistoricGridIterator {
-        return HistoricGridIterator(grid: self)
-    }
+    public func makeIterator() -> GridIterator { return GridIterator(grid: self) }
 }
 
-func gliderInitializer(row: Int, col: Int) -> CellState {
-    switch (row, col) {
-    case (0, 1), (1, 2), (2, 0), (2, 1), (2, 2): return .alive
-    default: return .empty
+public extension Grid {
+    public static func gliderInitializer(pos: GridPosition) -> CellState {
+        switch pos {
+        case (0, 1), (1, 2), (2, 0), (2, 1), (2, 2): return .alive
+        default: return .empty
+        }
     }
 }
-
